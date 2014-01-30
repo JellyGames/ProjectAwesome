48cd3d1c93492da994d057f65491a85259fd747e
allanwilbers <allan_Ferrari@hotmail.com>
2014-01-30 10:57:36 +0100

    deleteee

--- Tutorial/GameObject.cpp
+++ Tutorial/GameObject.cpp
@@ -1,10 +1,13 @@
 #include "GameObject.h"
 
-
-GameObject::GameObject(void)
+GameObject::GameObject(char* texturePath,SDL_Rect sourceRectangle,SDL_Rect destRect,SDL_Renderer* renderer)
 {
-}
 
+	sRect = sourceRectangle;
+	dRect = destRect;
+	
+
+};
 
 GameObject::~GameObject(void)
 {
--- Tutorial/GameObject.h
+++ Tutorial/GameObject.h
@@ -5,14 +5,30 @@
 #include <stdlib.h>
 #include <iostream>
 #include <Windows.h>
-
+#include <SDL_image.h>
 
 
 class GameObject
 {
 public:
-	GameObject(void);
+	//methods
+	GameObject(char* texturePath,SDL_Rect sourceRectangle,SDL_Rect destRect,SDL_Renderer* renderer);
 	~GameObject(void);
+	
+	void Draw(SDL_Renderer* renderer);
+	void Init();
+
+	//fields
+	SDL_Texture* texture;
+	//sprite size and offset for animations
+	SDL_Rect sRect;
+	//position and actual size on screen
+	SDL_Rect dRect;
+
+	
+
 };
+
+
 #endif 
 
