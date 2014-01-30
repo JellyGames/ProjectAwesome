786a37772a6297c90a4b85b1ceaddcbb703e06b1
allanwilbers <allan_Ferrari@hotmail.com>
2014-01-30 10:37:51 +0100

    GameObject basic
    sett al includes and new gameobject class

---
+++ Tutorial/GameObject.h
@@ -0,0 +1,18 @@
+#ifndef _GAMEOBJECT_H_
+#define _GAMEOBJECT_H_
+
+#include <SDL.h>
+#include <stdlib.h>
+#include <iostream>
+#include <Windows.h>
+
+
+
+class GameObject
+{
+public:
+	GameObject(void);
+	~GameObject(void);
+};
+#endif 
+
