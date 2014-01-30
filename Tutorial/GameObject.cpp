1fe9ff7159acc1247a24b8630ed7949e00dcece5
allanwilbers <allan_Ferrari@hotmail.com>
2014-01-30 11:35:09 +0100

    added draw and check textrure
    lalalal hielkeeee

--- Tutorial/GameObject.cpp
+++ Tutorial/GameObject.cpp
@@ -1,4 +1,6 @@
+using namespace std;
 #include "GameObject.h"
+
 
 GameObject::GameObject(char* texturePath,SDL_Rect sourceRectangle,SDL_Rect destRect,SDL_Renderer* renderer)
 {
@@ -6,9 +8,20 @@ GameObject::GameObject(char* texturePath,SDL_Rect sourceRectangle,SDL_Rect destR
 	sRect = sourceRectangle;
 	dRect = destRect;
 	texture = IMG_LoadTexture(renderer,texturePath);
-
+	if(texture == NULL){
+		cout << "texture is 0" << endl;
+	}
+	Init();
 };
 
+void GameObject::Init()
+{
+
+};
+void GameObject::Draw(SDL_Renderer* renderer)
+{
+	SDL_RenderCopy(renderer,texture,&sRect,&dRect);
+}
 GameObject::~GameObject(void)
 {
-}
+};
